842e5892aedd7b7fd88004f65794d35a576a75ae
hamzapamuk <h.pmk236023@gmail.com>
2021-07-29 16:30:58 +0300

    deneme

---
+++ src/main/java/com/example/school/entity/School.java
@@ -0,0 +1,76 @@
+package com.example.school.entity;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "school")
+public class School  
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false,length = 6)
+    private String code;
+
+    @Column(columnDefinition="boolean default false",nullable = false)
+    private boolean active;
+
+    @OneToMany(cascade = CascadeType.PERSIST, mappedBy = "school")
+    private List<Student> students;
+
+ 
+    public School(){}
+
+    public School(Long id, String name, String code, Boolean active) {
+        this.id = id;
+        this.name = name;
+        this.code = code;
+        this.active = active;
+        
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+}
