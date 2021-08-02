fa373df465ab40bde2f1802e120c09face1a31f8
hamzapamuk <h.pmk236023@gmail.com>
2021-08-02 17:36:08 +0300

    commit

--- src/main/java/com/example/school/repository/SchoolRepository.java
+++ src/main/java/com/example/school/repository/SchoolRepository.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import com.example.school.entity.School;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,8 +14,8 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 @RepositoryRestResource(path = "school")
 public interface SchoolRepository extends JpaRepository<School,Long> 
 {
-    @Query("SELECT s from School s WHERE LOWER(s.name) LIKE LOWER(concat('%', concat(:name, concat('%')))) AND LOWER(s.name) LIKE LOWER(concat('%', concat(:code,concat('%'))))")
-    List<School> findBySchoolName(@Param("name") String name,@Param("code") String code);
+    @Query("SELECT s from School s WHERE LOWER(s.name) LIKE LOWER(concat('%', concat(:name, concat('%')))) AND LOWER(s.code) LIKE LOWER(concat('%', concat(:code,concat('%'))))")
+    Page<School> findBySchoolName(@Param("name") String name,@Param("code") String code, Pageable p);
 
 
     List<School> findByActiveTrue();
--- src/main/java/com/example/school/repository/StudentRepository.java
+++ src/main/java/com/example/school/repository/StudentRepository.java
@@ -10,7 +10,11 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 @RepositoryRestResource(path = "student")
 public interface StudentRepository extends JpaRepository<Student,Long>
 {
-    @Query("SELECT s from Student s WHERE LOWER(s.name) LIKE LOWER(concat('%', concat(:name,concat('%')))) AND LOWER(s.surname) LIKE LOWER(concat('%', concat(:surname,concat('%')))) AND LOWER(s.number) LIKE LOWER(concat('%', concat(:number,concat('%')))) AND LOWER(s.school.name) LIKE LOWER(concat('%', concat(:schoolName,concat('%')))) AND (s.age=:age OR s.age is null) AND (s.gpa=:gpa OR s.gpa is null) " )
+    @Query("SELECT s from Student s LEFT OUTER JOIN s.school sc WHERE LOWER(s.name) LIKE LOWER(concat('%', concat(:name,concat('%')))) AND "
+    +"LOWER(s.surname) LIKE LOWER(concat('%', concat(:surname,concat('%')))) AND " 
+    +"LOWER(s.number) LIKE LOWER(concat('%', concat(:number,concat('%')))) AND "
+    +"(:schoolName='' OR LOWER(sc.name) LIKE LOWER(concat('%', concat(:schoolName,concat('%'))))) AND "
+    +"(s.gpa=:gpa OR :gpa is null) AND (s.age=:age OR :age is null)" )
     List<Student> findAllSearch(@Param("name") String name,
         @Param("surname") String surname,
         @Param("number") String number,
