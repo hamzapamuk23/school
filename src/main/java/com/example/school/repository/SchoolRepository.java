ea35d588c092eef123edcde9b182df2eaa8a6029
hamzapamuk <h.pmk236023@gmail.com>
2021-09-29 16:39:05 +0300

    commit

--- src/main/java/com/example/school/repository/SchoolRepository.java
+++ src/main/java/com/example/school/repository/SchoolRepository.java
@@ -1,9 +1,7 @@
 package com.example.school.repository;
 
 import java.util.List;
-
 import com.example.school.entity.School;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -17,6 +15,7 @@ public interface SchoolRepository extends JpaRepository<School,Long>
     @Query("SELECT s from School s WHERE LOWER(s.name) LIKE LOWER(concat('%', concat(:name, concat('%')))) AND LOWER(s.code) LIKE LOWER(concat('%', concat(:code,concat('%'))))")
     Page<School> findBySchoolName(@Param("name") String name,@Param("code") String code, Pageable p);
 
+    Page<School> OrderByName(Pageable p);
 
     List<School> findByActiveTrue();
 }
